7345e5aa90902974875856f9db374d3dd3cd330f
leslie-ht <huiqing.tong@hotmail.com>
2016-05-03 22:19:42 +0800

    update Chapter3
    add author info

--- src/chapter3/Main.java
+++ src/chapter3/Main.java
@@ -2,7 +2,10 @@ package chapter3;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
+/*
+ * author: leslie
+ * date: 3 May 2016
+ */
 public class Main {
 
 	public static void main(String[] args) {
